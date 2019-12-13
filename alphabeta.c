804936a4bdac83675c154ed395d8eedf9795a344
ryota050505 <05ryouta05@gmail.com>
2019-12-13 23:21:33 +0900

    fix typo

--- alphabeta.c
+++ alphabeta.c
@@ -2,28 +2,27 @@
 #include <stdlib.h>
 #include <limits.h>
 
-int maxlevel(int deepth, int x, int y);
-int minlevel(int deepth, int x, int y);
+int maxlevel(int depth, int x, int y);
+int minlevel(int depth, int x, int y);
 
 extern int board[15][15];
 extern int dx[8];
 extern int dy[8];
 
-int maxlevel(int deepth, int x, int y){
+int maxlevel(int depth, int x, int y){
     board[y][x] = 1;        //仮に置く(自分)
     int max = INT_MIN;
 	int score;
 
-	if(deepth == 0){            //一番低い子ノード
+	if(depth == 0){            //一番低い子ノード
         //評価
 		if(!ban_judge(x, y))return -100;    //禁じ手-100
         else return 100;                    //その他+100(適当)
 	}
 
-    
 	for(int i = 0; i < 8; i++){             //注目しているマスの周り8マスを調査
         if(board[y+dy[i]][x+dx[i]])continue;        //既にあったらパス
-        score = minlevel(deepth-1, x+dx[i], y+dy[i]);
+        score = minlevel(depth-1, x+dx[i], y+dy[i]);
         if(score >= max){
             max = score;
         }
@@ -31,12 +30,12 @@ int maxlevel(int deepth, int x, int y){
     return max;
 }
 
-int minlevel(int deepth, int x, int y){
+int minlevel(int depth, int x, int y){
     board[y][x] = 2;        //仮に置く(相手)
     int min = INT_MAX;
     int score;
 
-    if(deepth == 0){            //一番低い子ノード
+    if(depth == 0){            //一番低い子ノード
         //評価(相手)
 		if(!ban_judge(x, y))return 100;    //禁じ手+100
         else return -100;                  //その他-100(適当)
@@ -44,7 +43,7 @@ int minlevel(int deepth, int x, int y){
 
     for(int i = 0; i < 8; i++){
         if(board[y+dy[i]][x+dx[i]])continue;
-        score = maxlevel(deepth-1, x+dx[i], y+dy[i]);
+        score = maxlevel(depth-1, x+dx[i], y+dy[i]);
         if(score <= min){
             min = score;
         }
