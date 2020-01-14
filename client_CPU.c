86d25ad7235ef2ce878cff2e6b8956431d2d1880
ryota050505 <05ryouta05@gmail.com>
2020-01-14 19:43:12 +0900

    fix typo ban_judgeのi,j

--- client_CPU.c
+++ client_CPU.c
@@ -190,7 +190,7 @@ int main(void) {
 				for(i = 0; i < 15; i++){
 					for(j = 0; j < 15; j++){
 						if(board[i][j]){/*printf("#,  ");*/continue;}
-						if(!ban && !ban_judge(i,j,MY_NUM)){/*printf("b, ");*/continue;}
+						if(!ban && !ban_judge(j,i,MY_NUM)){/*printf("b, ");*/continue;}
 						int score = get_value(j, i, MY_NUM) + get_value(j, i, ENEMY_NUM);
 						if(max <= score){
 							max = score;
